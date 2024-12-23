ed8663aa0ba3d181f2d61ad1817d9e7d84b98b3a
Ben Miles <benmilesGM@gmail.com>
2024-12-23 22:07:49 +0000

    refactors code in line with JSLint suggestions

--- assets/script.js
+++ assets/script.js
@@ -4,40 +4,60 @@ gsap.registerPlugin(Flip);
 
 // Array of all cards in the deck
 
-const fullDeck = ["00-fool.jpg", "01-magician.jpg", "02-highpriestess.jpg", "03-empress.jpg", "04-emperor.jpg", "05-hierophant.jpg", 
-    "06-lovers.jpg", "07-chariot.jpg", "08-strength.jpg", "09-hermit.jpg", 
-    "10-wheeloffortune.jpg", "11-justice.jpg", "12-hangedman.jpg", "13-death.jpg", "14-temperance.jpg", 
-    "15-devil.jpg", "16-tower.jpg", "17-star.jpg", "18-moon.jpg", "19-sun.jpg",
-    "20-judgement.jpg", "21-world.jpg", "22-wands-01.jpg", "23-wands-02.jpg", "24-wands-03.jpg", 
-    "25-wands-04.jpg", "26-wands-05.jpg", "27-wands-06.jpg", "28-wands-07.jpg", "29-wands-08.jpg",
-    "30-wands-09.jpg", "31-wands-10.jpg", "32-wands-11.jpg", "33-wands-12.jpg", "34-wands-13.jpg", 
-    "35-wands-14.jpg", "36-pentacles-01.jpg", "37-pentacles-02.jpg", "38-pentacles-03.jpg", "39-pentacles-04.jpg",
-    "40-pentacles-05.jpg", "41-pentacles-06.jpg", "42-pentacles-07.jpg", "43-pentacles-08.jpg", "44-pentacles-09.jpg", 
-    "45-pentacles-10.jpg", "46-pentacles-11.jpg", "47-pentacles-12.jpg", "48-pentacles-13.jpg", "49-pentacles-14.jpg",
-    "50-cups-01.jpg", "51-cups-02.jpg", "52-cups-03.jpg", "53-cups-04.jpg", "54-cups-05.jpg", 
-    "55-cups-06.jpg", "56-cups-07.jpg", "57-cups-08.jpg", "58-cups-09.jpg", "59-cups-10.jpg",
-    "60-cups-11.jpg", "61-cups-12.jpg", "62-cups-13.jpg", "63-cups-14.jpg", "64-swords-01.jpg", 
-    "65-swords-02.jpg", "66-swords-03.jpg", "67-swords-04.jpg", "68-swords-05.jpg", "69-swords-06.jpg",
-    "70-swords-07.jpg", "71-swords-08.jpg", "72-swords-09.jpg", "73-swords-10.jpg", "74-swords-11.jpg", 
-    "75-swords-12.jpg", "76-swords-13.jpg", "77-swords-14.jpg"];
+const fullDeck = ["00-fool.jpg", "01-magician.jpg",
+    "02-highpriestess.jpg", "03-empress.jpg", "04-emperor.jpg",
+    "05-hierophant.jpg", "06-lovers.jpg", "07-chariot.jpg",
+    "08-strength.jpg", "09-hermit.jpg", "10-wheeloffortune.jpg",
+    "11-justice.jpg", "12-hangedman.jpg", "13-death.jpg",
+    "14-temperance.jpg", "15-devil.jpg", "16-tower.jpg",
+    "17-star.jpg", "18-moon.jpg", "19-sun.jpg",
+    "20-judgement.jpg", "21-world.jpg", "22-wands-01.jpg",
+    "23-wands-02.jpg", "24-wands-03.jpg", "25-wands-04.jpg",
+    "26-wands-05.jpg", "27-wands-06.jpg", "28-wands-07.jpg",
+    "29-wands-08.jpg", "30-wands-09.jpg", "31-wands-10.jpg",
+    "32-wands-11.jpg", "33-wands-12.jpg", "34-wands-13.jpg",
+    "35-wands-14.jpg", "36-pentacles-01.jpg", "37-pentacles-02.jpg",
+    "38-pentacles-03.jpg", "39-pentacles-04.jpg", "40-pentacles-05.jpg",
+    "41-pentacles-06.jpg", "42-pentacles-07.jpg", "43-pentacles-08.jpg",
+    "44-pentacles-09.jpg", "45-pentacles-10.jpg", "46-pentacles-11.jpg",
+    "47-pentacles-12.jpg", "48-pentacles-13.jpg", "49-pentacles-14.jpg",
+    "50-cups-01.jpg", "51-cups-02.jpg", "52-cups-03.jpg",
+    "53-cups-04.jpg", "54-cups-05.jpg", "55-cups-06.jpg",
+    "56-cups-07.jpg", "57-cups-08.jpg", "58-cups-09.jpg",
+    "59-cups-10.jpg", "60-cups-11.jpg", "61-cups-12.jpg",
+    "62-cups-13.jpg", "63-cups-14.jpg", "64-swords-01.jpg",
+    "65-swords-02.jpg", "66-swords-03.jpg", "67-swords-04.jpg",
+    "68-swords-05.jpg", "69-swords-06.jpg", "70-swords-07.jpg",
+    "71-swords-08.jpg", "72-swords-09.jpg", "73-swords-10.jpg",
+    "74-swords-11.jpg", "75-swords-12.jpg", "76-swords-13.jpg",
+    "77-swords-14.jpg"];
 
 // Array describing of all cards in the deck
 
-const fullDeckAlt = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant", 
-    "The Lovers", "The Chariot", "Strength", "The Hermit", 
-    "The Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance", 
+const fullDeckAlt = ["The Fool", "The Magician",
+    "The High Priestess", "The Empress", "The Emperor",
+    "The Hierophant", "The Lovers", "The Chariot",
+    "Strength", "The Hermit", "The Wheel of Fortune",
+    "Justice", "The Hanged Man", "Death", "Temperance",
     "The Devil", "The Tower", "The Star", "The Moon", "The Sun",
-    "Judgement", "The World", "Ace of Wands", "Two of Wands", "Three of Wands", 
-    "Four of Wands", "Five of Wands", "Six of Wands", "Seven of Wands", "Eight of Wands",
-    "Nine of Wands", "Ten of Wands", "Page of Wands", "Knight of Wands", "Queen of Wands", 
-    "King of Wands", "Ace of Pentacles", "Two of Pentacles", "Three of Pentacles", "Four of Pentacles",
-    "Five of Pentacles", "Six of Pentacles", "Seven of Pentacles", "Eight of Pentacles", "Nine of Pentacles", 
-    "Ten of Pentacles", "Page of Pentacles", "Knight of Pentacles", "Queen of Pentacles", "Kingof Pentacles",
-    "Ace of cups", "Two of cups", "Three of cups", "Four of cups", "Five of cups", 
-    "Six of cups", "Seven of cups", "Eight of cups", "Nine of cups", "Ten of cups",
-    "Page of cups", "Knight of cups", "Queen of cups", "King of cups", "Ace of Swords", 
-    "Two of Swords", "Three of Swords", "Four of Swords", "Five of Swords", "Six of Swords",
-    "Seven of Swords", "Eight of Swords", "Nine of Swords", "Ten of Swords", "Page of Swords", 
+    "Judgement", "The World", "Ace of Wands", "Two of Wands",
+    "Three of Wands", "Four of Wands", "Five of Wands",
+    "Six of Wands", "Seven of Wands", "Eight of Wands",
+    "Nine of Wands", "Ten of Wands", "Page of Wands",
+    "Knight of Wands", "Queen of Wands", "King of Wands",
+    "Ace of Pentacles", "Two of Pentacles", "Three of Pentacles",
+    "Four of Pentacles", "Five of Pentacles", "Six of Pentacles",
+    "Seven of Pentacles", "Eight of Pentacles", "Nine of Pentacles",
+    "Ten of Pentacles", "Page of Pentacles", "Knight of Pentacles",
+    "Queen of Pentacles", "Kingof Pentacles", "Ace of cups",
+    "Two of cups", "Three of cups", "Four of cups",
+    "Five of cups", "Six of cups", "Seven of cups",
+    "Eight of cups", "Nine of cups", "Ten of cups",
+    "Page of cups", "Knight of cups", "Queen of cups",
+    "King of cups", "Ace of Swords", "Two of Swords",
+    "Three of Swords", "Four of Swords", "Five of Swords",
+    "Six of Swords", "Seven of Swords", "Eight of Swords",
+    "Nine of Swords", "Ten of Swords", "Page of Swords",
     "Knight of Swords", "Queen of Swords", "King of Swords"];
 
 // deck object
@@ -96,7 +116,7 @@ let invertSelect = "random";
 function displayRadioValue() {
     let ele = document.getElementsByName("options");
 
-    for (i = 0; i < ele.length; i++) {
+    for (i = 0; i < ele.length; i+=1) {
         if (ele[i].checked)
         {invertSelect = ele[i].value;}
     }
